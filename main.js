2bc82660655996ea628c601b281bd58fbe8b58cc
Emeraldboy-whtjr <82824596+Emeraldboy-whtjr@users.noreply.github.com>
2021-12-19 22:46:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+Webcam.set({
+    width : 350,
+    height : 300,
+    image_format : "png",
+    png_quality : 90,
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function snapshot(){
+    Webcam.snap(function(data_URL){
+        document.getElementById("result").innerHTML = "<img id='capture_image' src='" + data_URL + "'>"
+    });
+}
+
+console.log("ml5 version " , ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/eHImFqYEF/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("model loaded");
+}
+
+function check(){
+    image = document.getElementById("capture_image");
+    classifier.classify(image,gotResult);
+
+}
+
+function gotResult(error,results){
+    if (error){
+        console.log("error");
+    }
+    else {
+        console.log(results);
+        document.getElementById("object-name").innerHTML = results[0].label;
+        document.getElementById("object-accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
